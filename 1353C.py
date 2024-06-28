35c7a5ff01cce22b752cb55074e378a0bed43f70
Fardin Hassan Tamim <125815633+Tamieeem@users.noreply.github.com>
2024-06-28 21:12:39 +0600

    Update 1353C.py

--- 1353C.py
+++ 1353C.py
@@ -7,14 +7,15 @@ for i in range(t):
     elif n == 3:
         print(8)
     else:
-        formula = ((n+1)//2)
-        temp = n
-        mid = (temp+1)//2
-        for j in range(n-formula):
-            min_moves+=((temp**2) - ((temp-2)**2))*(temp - mid)
-            temp = (temp-2)
-            mid = (temp+1)//2
+        formula = ((n+1)//2)   #this simply gives us the center position(consider the center as that one cell where we want to move all the items/cells.
+        temp = n               #storing in in a temporary variable as we need to use it in the loop and manipulate it to reach our final output.
+        mid = (temp+1)//2      #the variable says what it really means, the middle position of n*n matrix.
+        for j in range(n-formula):      #loop should run untill you have 3*3 matrix cause this has only one outer cell. That's why we considered it as base case
+            min_moves+=((temp**2) - ((temp-2)**2))*(temp - mid)    #this is basically the algorithm to get the number of outer cell in a matrix. Do some work on paper, you will understand.
+            temp = (temp-2)                          #temp-2 because, our n is odd. so 7-2 is 5, 5-2 is 3, this is how the odd matrix continues in case you have larger matrix.
+            mid = (temp+1)//2                        #you need to get new mid each time you finish one outer cell 
         print(min_moves)
+        
         
         
         
